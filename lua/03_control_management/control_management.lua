3b051fa65bdb1dee26b4bbda5c425e6baeb29855
Martin Pavlas <martin@think-forth.com>
2021-05-18 23:15:32 +0200

    Adjust formatting of the script source code

--- lua/03_control_management/control_management.lua
+++ lua/03_control_management/control_management.lua
@@ -2,32 +2,32 @@
 
 
 -- Change visibility
-control = controls.get(1)
-controls.setVisible(control, false)
-print("is it still visible: " .. (controls.isVisible(control) and "YES" or "NO"))
+control = controls.get (1)
+controls.setVisible (control, false)
+print ("is it still visible: " .. (controls.isVisible (control) and "YES" or "NO"))
 
 
 -- Change name
-control = controls.get(7)
-controls.setName(control, "CHANGED")
-print("The new label is: " .. controls.getName(control))
+control = controls.get (7)
+controls.setName (control, "CHANGED")
+print ("The new label is: " .. controls.getName (control))
 
 
 -- Change color
-control = controls.get(13)
-controls.setColor(control, ORANGE)
-print("Is it blue now? " .. (controls.getColor(control) == BLUE and "YES" or "NO"))
+control = controls.get (13)
+controls.setColor (control, ORANGE)
+print ("Is it blue now? " .. (controls.getColor (control) == BLUE and "YES" or "NO"))
 
 
 -- Change a location, using the helpers.slotToBounds () function.
-control = controls.get(19)
-controls.setBounds(control, helpers.slotToBounds(6))
-bounds = controls.getBounds(control)
-print("New bounds are: x=" .. bounds[X] ..
+control = controls.get (19)
+controls.setBounds (control, helpers.slotToBounds (6))
+bounds = controls.getBounds (control)
+
+print ("New bounds are: x=" .. bounds[X] ..
     ", y=" .. bounds[Y] ..
     ", width=" .. bounds[WIDTH] ..
-    ", height=" .. bounds[HEIGHT]
-)
+    ", height=" .. bounds[HEIGHT])
 
 -- Change assignment of the control to a controlSet
 controls.setControlSet (control, 1)
