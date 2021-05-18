488a3b9ea76dfc9f526d809e90abf35bc007ea25
Martin Pavlas <martin@think-forth.com>
2021-05-18 14:42:41 +0200

    Fix typos in the helloworld.lua

--- lua/01_helloworld/helloworld.lua
+++ lua/01_helloworld/helloworld.lua
@@ -1,6 +1,6 @@
 -- Hello world example
 --
--- The text "Hellow MIDI world!" will be printed out to the ElectraOne console
+-- The text "Hello MIDI world!" will be printed out to the ElectraOne console
 
 -- Setup
-print ("Hello MIDI world"!)
+print ("Hello MIDI world!")
