70da3eb676df1ea01c5bf2cb734cd3f5d5c93047
Martin Pavlas <martin@think-forth.com>
2021-05-19 21:27:50 +0200

    Add an example of advance control manipulation

---
+++ lua/04_value_callbacks/related_controls.lua
@@ -0,0 +1,35 @@
+-- Display controls related to specific value of another control
+
+-- define assignment of controls to groups
+controlGroups = {
+    [0] = { 20, 21, 22 },
+    [1] = { 26, 27, 28 },
+    [2] = { 32, 33 }
+}
+
+-- a function to hide all controls within the groups
+function hideAllGroups (groups)
+    for groupId = 0, #groups do
+        for i, controlId in ipairs (groups[groupId]) do
+            control = controls.get (controlId)
+            control:setVisible (false)
+        end
+    end
+end
+
+-- show given control group
+function showGroup (groups, groupId)
+    for i, controlId in ipairs (groups[groupId]) do
+        control = controls.get (controlId)
+        control:setSlot (i + 1)
+    end
+end
+
+-- the callback function called from the preset
+function displayGroup (controlId, value)
+    hideAllGroups (controlGroups)
+    showGroup (controlGroups, value)
+end
+
+-- set the initial state. group 0 is displayed
+showGroup (controlGroups, 0)
